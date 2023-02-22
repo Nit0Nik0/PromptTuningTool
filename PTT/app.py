a8ec9039839900bedf65f3eb3fc2bac72ec5f606
Nit0Nik0 <52796883+Nit0Nik0@users.noreply.github.com>
2023-02-22 18:22:43 -0500

    Adding form logic for GPT-3 Section

--- PTT/app.py
+++ PTT/app.py
@@ -1,18 +1,28 @@
 #Some of the flask imports we will save for later.
 import os
-#import openai
+import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 # Uncomment next line when you need to use api
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+    
 
-@app.route("/GPT-3/")
+@app.route("/GPT-3/", methods=("GET", "POST"))
 def GPT3():
+    if request.method == "POST":
+        settings = request.form
+        
+        return redirect(url_for("index", result=response.choices[0].text))     
+
     return render_template("gpt-3.html")
 
 app.run(host="0.0.0.0", port = 5001, debug=True)
+
+
+# Helper Functions
+def make
--- todo.md
+++ todo.md
@@ -2,15 +2,6 @@
 
 ## GPT-3
 
-- [X] fix base.html nav bar
-- [X] Add GPT-3 html file
-- [X] build out form
-  - [X] input box
-  - [X] output box 
-  - [X] prompt
-  - [ ] response
-  - [x] model
-  - [ ] max_tokens
-  - [ ] top_p
-- Consider fine tune section with model selection.
-- Account settings section would be cash money
+- [ ] 
+- [ ] 
+- [ ] 
