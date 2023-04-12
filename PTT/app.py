8a013f91c7c8f044adfeb2b5318d8a27993f3a83
Nit0Nik0 <52796883+Nit0Nik0@users.noreply.github.com>
2023-04-12 15:08:43 -0400

    Finished up GPT-3 Portion
    Ended up finshing up the GPT-3 Portion of the app.

--- PTT/app.py
+++ PTT/app.py
@@ -1,8 +1,25 @@
 #Some of the flask imports we will save for later.
 import os
+from dotenv import load_dotenv
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
+
+
+def buildRequest(settings):    
+    response = openai.Completion.create(
+        model = settings["model"],
+        prompt = settings["prompt"],
+        max_tokens = int (settings["max_tokens"]),
+        temperature = float(settings["temp"]),
+        top_p = float(settings["top_p"]),
+        frequency_penalty = float(settings["frequency_penalty"]),
+        presence_penalty =float(settings["presence_penalty"])
+    )
+    return response
+    
+
+load_dotenv()
 app = Flask(__name__)
 # Uncomment next line when you need to use api
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -11,27 +28,13 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     return render_template("index.html")
     
-
 @app.route("/GPT-3/", methods=("GET", "POST"))
 def GPT3():
     if request.method == "POST":
         settings = request.form
         response = buildRequest(settings)
-        return redirect(url_for("index", result=response.choices[0].text))     
-    return render_template("gpt-3.html")
+        return redirect(url_for("GPT3", result=response.choices[0].text))
+    result = request.args.get("result") 
+    return render_template("gpt-3.html", result=result)
 
 app.run(host="0.0.0.0", port = 5001, debug=True)
-
-
-# Helper Functions
-def buildRequest(settings):
-    #form names
-    print(settings)
-    """
-    model
-    max_tokens
-    temp
-    top_p
-    frequency_penalty
-    presence_penalty
-    """
--- todo.md
+++
@@ -1,4 +0,0 @@
-# todo list
-- [ ] add inputs for GPT-3 tp their own input class.
-- [ ] add requst logic for gpt-3.
-- [ ] worry about error handeling latter
